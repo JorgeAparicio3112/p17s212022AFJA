2aca031c97741ab01e29cc0519c96fe50ad2bcbe
JorgeAparicio3112 <revenant2314@gmail.com>
2022-09-28 11:49:45 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/p17s212022afja/MainActivity2.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.p17s212022afja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
